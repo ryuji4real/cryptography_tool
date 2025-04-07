2481fe84be51ff90ab5e77b65f1d99d0670a1ddb
wdd <ryu4x4@gmail.com>
2025-04-08 00:39:14 +0200

    Create monoalphabetique.h

---
+++ include/monoalphabetique.h
@@ -0,0 +1,7 @@
+#ifndef MONOALPHABETIQUE_H
+#define MONOALPHABETIQUE_H
+
+void chiffrement_monoalphabetique(char *message, const char *alphabet_chiffre);
+void dechiffrement_monoalphabetique(const char *message, char *resultat, const char *alphabet_chiffre);
+
+#endif
